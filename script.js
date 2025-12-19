0e72b5da3c444f2891ad9f9d06fd842a7294596c
rendonbarco727-coder <rendonbarco727@gmail.com>
2025-12-19 14:52:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const series = [
   },
   {
     titulo: "IT Temporada 1 - Episodio 2",
-    id: "109rkG4sPdh38wXWjAca6YAldG7AK2amo",
+    id: "1UM9Pl6JP00ruYSsglltJ6yx3FvGCLKn",
     portada: "img/It.jpg"
   },
   {
