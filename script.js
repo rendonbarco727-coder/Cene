234552abaff367e219ffc42489e916e3b6844324
rendonbarco727-coder <rendonbarco727@gmail.com>
2026-01-01 19:58:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -25,6 +25,14 @@ const peliculas = [
   { titulo:"Cosas imposibles",id:"1S-O5BiPPZYUl9nVUicPAYywXhIKqM1VP",portada:"img/cosas.jpg"},
   
   {titulo:"Caramelo",id:"1dn-DmNz7NZTmz0EvFIHuMMNeTSdmZHS3",portada:"img/Caramelo.jpg"},
+
+  {titulo:"Asi en la Tierra como en el Infierno",id:"1A8_ndAD0G5jjTuYEJqRZQWuGEe4kOxAx",portada:"img/asi.jpg"},
+
+  {titulo:"Gretel y Hansel",id:"1xHYWt6QpCDf84dUB8DOdJtsEl04T87qd",portada:"img/gretel.jpg"},
+
+  {titulo:"Apocalypto",id:"1Zq8sNv1350sF6vmzZLerULFZ3E64hike",portada:"img/apocalipto.jpg"},
+  
+  {titulo:"Los 4 Fantasticos",id:"1m7c5HjR2OcT5KfBJrLL9o4TK8dTzjL4U",portada:"img/4fan.jpg"},
   
   {titulo:"Pearl Harbor",id:"1pdHWRgVOVA6-PYDbtxh9T6KrJteyr9Nr",portada:"img/pear.jpg"}
 ];
